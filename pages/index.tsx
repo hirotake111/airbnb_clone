9f1fec51ae850197508371d5b04376dff7787626
Hiro <hiroshi.takeuchi111@gmail.com>
2021-12-18 22:26:10 +0000

    updated next trip component styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,12 @@ import type { NextPage } from "next";
 
 import { useSearch } from "../hooks/searchHook";
 import Nav from "../components/Nav/Nav/Nav";
-
-import styles from "../styles/Home.module.css";
 import GiftCard from "../components/giftcard/GiftCard/GiftCard";
 import Hero from "../components/hero/Hero";
 import NextTrip from "../components/nextTrip/NextTrip";
 import MobileMenu from "../components/mobileMenu/MobileMenu/MobileMenu";
+
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   const { disableSearch, scrolled } = useSearch();
@@ -32,9 +32,7 @@ const Home: NextPage = () => {
           <Hero />
         </div>
         <GiftCard />
-        <div className={styles.nextTrip__container}>
-          <NextTrip />
-        </div>
+        <NextTrip />
         <div className={styles.mobileMenu}>
           <MobileMenu />
         </div>
